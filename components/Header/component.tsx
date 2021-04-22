b30c51cf85f9b0cfce595f4472a689af7d209d7a
tweetliable <tweetliable@gmail.com>
2021-04-22 23:29:04 +0630

    Add Header

--- components/Header/component.tsx
+++ components/Header/component.tsx
@@ -1,3 +1,11 @@
 export const Header = ({}) => {
-  return <div>Header</div>;
+  return (
+  <header>
+      <div className="inline-flex align-middle bg-blue-300 w-screen">
+        <h1 className="p-4 text-2xl"> Tweetliable </h1>
+        <div className="p-5"> Home </div>
+        <div className="p-5"> About </div>
+      </div>
+    </header>
+  );
 };
